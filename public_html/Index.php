07de637e6c13f6eb34bb0aa5affda18688d3f417
Kiran94 <kiran_patel94@hotmail.com>
2016-06-20 22:10:21 +0100

    readded comments

--- public_html/Index.php
+++ public_html/Index.php
@@ -16,5 +16,28 @@
     $array = $TripService->GetTripTotals(); 
 
     echo var_dump($array); 
+
+    //$currentLocation = $TripService->Get('b6dc24ec-3728-11e6-a680-8c15c1896d85'); 
+    
+    // echo $currentLocation->ID; 
+    // echo "<br/>";
+    // echo $currentLocation->Name; 
+
+
+    // $trips = new services\TripService($repo, $logger);
+    // $res = $trips->Get("58b3ab6e-36c5-11e6-bbd4-f388cc669bd7");
+
+    // if(!$res)
+    // {
+    //     echo "failed to find trip"; 
+    //     exit(); 
+    // }
+
+    // echo $res->ID; 
+    // echo "<br/>";
+     
+    // echo $res->Name;
+    // echo "<br/>"; 
+
     echo "done"; 
 ?>
