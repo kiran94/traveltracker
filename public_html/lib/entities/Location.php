6b290832d347369a0a275025fcf371fdd089c573
Kiran94 <kiran_patel94@hotmail.com>
2016-06-20 22:14:47 +0100

    added documentation comments for Location entity

--- public_html/lib/entities/Location.php
+++ public_html/lib/entities/Location.php
@@ -6,12 +6,25 @@ namespace entities;
 */
 class Location 
 {
+    /* Unique identifier for Location */
     public $ID; 
+
+    /* Latitude of the Location */
     public $Lat; 
+
+    /* Longitude of the Location */
     public $Long; 
+
+    /* Label of the Location */
     public $Name; 
-    public $Date; 
+
+    /* Date of the Location (timestamp) */
+    public $Date;
+
+    /* Flag indicating if the Location is deleted or not */ 
     public $IsDeleted; 
+
+    /* Trip object this is related too */
     public $TripID; 
 }
 ?>
