30e60ffff27d330ee91338633f97448705c6f0d8
Kiran94 <kiran_patel94@hotmail.com>
2016-06-20 22:06:07 +0100

    added Location Entity

---
+++ public_html/lib/entities/Location.php
@@ -0,0 +1,17 @@
+<?php 
+namespace entities; 
+
+/*
+    Encapsulates information for a location object
+*/
+class Location 
+{
+    public $ID; 
+    public $Lat; 
+    public $Long; 
+    public $Name; 
+    public $Date; 
+    public $IsDeleted; 
+    public $TripID; 
+}
+?>
