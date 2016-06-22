97b9a4c3d70fe02b95588dd3e97e3badd8b13776
Kiran94 <kiran_patel94@hotmail.com>
2016-06-22 22:12:20 +0100

    added some generic javascript map functions + GetTripLocations logic

--- public_html/scripts/maps.js
+++ public_html/scripts/maps.js
@@ -1,12 +1,67 @@
+var map; 
+var markers = []; 
+
 /*
   Initialises the map with a default location
 */
 function initMap() 
 {
-  var map = new google.maps.Map(document.getElementById('map'), 
+  map = new google.maps.Map(document.getElementById('map'), 
   {
-    center: {lat: -8.4543386, lng: 114.5110503},
-    scrollwheel: true,
-    zoom: 8
+    center: {lat: 51.5331414, lng: -0.4773218},
+    scrollwheel: false,
+    zoom: 10
   });
+}
+
+/*
+  Adds a marker to the global map with the name and location
+*/
+function addMarker(name, location)
+{
+  var marker = new google.maps.Marker({
+    title: name,
+    position : location, 
+    map : map,
+    animation: google.maps.Animation.DROP,
+  });
+  
+  markers.push(marker); 
+}
+
+/*
+  Removes all markers from the map
+*/
+function clearMarkers()
+{
+  setMapOnAll(null); 
+  markers = []; 
+}
+
+/*
+  Gets Locations realted to the current trip
+*/
+function GetTripLocations()
+{
+  var tripID = document.getElementById('currentTrip').innerHTML; 
+  //// jquery post to a php endpoint with the trip id and return the locations related
+
+  $.ajax(
+  {
+    url: 'GetLocations.php',
+    type: "POST",
+    data: { TripID : tripID },
+    dataType: 'json',
+    success: function(data, status, jq)
+    {
+      console.log(data); 
+      console.log(status); 
+      console.log(jq); 
+    },
+    error: function(jq, err, thrown)
+    {
+      console.log("error " + err); 
+    }
+  });
+
 }
