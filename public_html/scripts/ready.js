1fdf72c7a6971cdf2eb415cc13e7bacfee58e5e1
Kiran94 <kiran_patel94@hotmail.com>
2016-06-23 20:45:33 +0100

    removed test script

--- public_html/scripts/ready.js
+++ public_html/scripts/ready.js
@@ -1,15 +1,11 @@
-/// FOR TESTING
 $( document ).ready(function()
 {
-  document.getElementById('currentTrip').innerHTML = "58b3ab6e-36c5-11e6-bbd4-f388cc669bd7"; 
+  //// run for default location
   GetTripLocations();
 
   $('.trip').on('click', function()
   {
-      var tripID = $(this).attr('id'); 
-      document.getElementById('currentTrip').innerHTML = tripID; 
+      document.getElementById('currentTrip').innerHTML = $(this).attr('id'); 
       GetTripLocations();
   }); 
-
-
 }); 
