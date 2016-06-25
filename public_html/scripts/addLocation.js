571b295f948dc2b17462082ffffc41e201bf1c9c
Kiran94 <kiran_patel94@hotmail.com>
2016-06-25 20:35:55 +0100

    some refactoring to the add location script to make it more modular

--- public_html/scripts/addLocation.js
+++ public_html/scripts/addLocation.js
@@ -1,29 +1,59 @@
+// AJAX loader
+var loader; 
+var submitted; 
+var lat = $("#lat"); 
+var lng = $('#lng'); 
+
 $(document).ready(function()
+{
+    registerGetLocationButton(); 
+
+    /// Need some validation front end to stop submission when lat and long are not provided
+    //registerFormValidation(); 
+});
+
+/*
+    Registers the click event on the get location button to get location and set to elements 
+*/
+function registerGetLocationButton()
 {
     $('#location').on('click', function(e)
     {
         e.preventDefault();
 
-        var loader = $('#loader'); 
+        loader = $('#loader'); 
         loader.css('display', 'block'); 
-       
-        if (navigator.geolocation)
-        {
-            var location = navigator.geolocation.getCurrentPosition(function(pos)
-            {
-                $("#lat").val(pos.coords.latitude);  
-                document.getElementById('lat_label').innerHTML = pos.coords.latitude; 
-
-                $('#lng').val(pos.coords.longitude); 
-                document.getElementById('lng_label').innerHTML = pos.coords.longitude; 
-
-                loader.css('display', 'none'); 
-            });
-        }
-        else
-        {
-             document.getElementById('lat_label').innerHTML = "Error"; 
-             document.getElementById('lng_label').innerHTML = "Error"; 
-        }
+        getLocation(); 
     }); 
-}); 
+}
+
+/*
+    Gets location of the users current position else sets labels to error
+*/
+function getLocation()
+{
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(SetLocation);
+    }
+    else
+    {
+        document.getElementById('lat_label').innerHTML = "Error"; 
+        document.getElementById('lng_label').innerHTML = "Error"; 
+    }
+}
+
+/*
+    Sets the location to the relavant DOM elements
+    @param: current geolocation
+*/
+function SetLocation(pos)
+{
+    lat.val(pos.coords.latitude);  
+    document.getElementById('lat_label').innerHTML = "Lat: " + pos.coords.latitude; 
+
+    lng.val(pos.coords.longitude); 
+    document.getElementById('lng_label').innerHTML = "Long: " + pos.coords.longitude; 
+
+    loader.css('display', 'none'); 
+}
