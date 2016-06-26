52ab2c1ae35bac2ed98f76fba49b31606a73a0ac
Kiran94 <kiran_patel94@hotmail.com>
2016-06-26 10:00:41 +0100

    closed off html meta tags

--- public_html/Index.php
+++ public_html/Index.php
@@ -5,9 +5,9 @@
 <!DOCTYPE html> 
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     
     <title>TravelTracker</title>
     <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Open+Sans'/>
