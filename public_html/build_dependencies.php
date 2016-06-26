de741bc85715f7532ec6f0e76bb1a52616d217e7
Kiran94 <kiran_patel94@hotmail.com>
2016-06-26 10:01:54 +0100

    added system timezone config and set in build dependencies

--- public_html/build_dependencies.php
+++ public_html/build_dependencies.php
@@ -6,8 +6,11 @@
     require_once 'lib/Connection.php'; 
     require_once 'lib/log4php/LoggerService.php'; 
     require_once 'lib/Repository.php'; 
-    
+
     $configService = new services\ConfigurationService("config.ini"); 
+    $timezone = $configService->Get("TimeZone", "Europe/London"); 
+    date_default_timezone_set($timezone);
+
     $logger = new services\LoggerService($configService); 
     $connection = new dataaccess\Connection($configService, $logger); 
     $repository = new dataaccess\Repository($connection, $logger); 
